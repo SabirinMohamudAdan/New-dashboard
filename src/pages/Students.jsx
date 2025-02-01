966ec62b56061b55b2bf736970e38990f90bfc24
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-01-31 22:36:50 -0800

    u

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -2,14 +2,14 @@ const Students = () => {
     return <div className=" sm:flex sm:gap-8 sm:pt-40 sm:ml-100 pt-100 
      mb-4 ml-32 block">
     <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 ">
-        <h1 className="text-4xl text-white text-center">Total students</h1>
+        <h1 className="text-4xl text-white text-center"> students page</h1>
     </div>
-    <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 mt-2 ">
+    {/* <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 mt-2 ">
         <h1 className="text-4xl text-white text-center">Active student</h1>
     </div>
     <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 mt-2">
         <h1 className="text-4xl text-white text-center">courses</h1>
-    </div>
+    </div> */}
 </div>
 };
 
--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -2,7 +2,7 @@ const Teachers = () => {
     return <div className=" sm:flex sm:gap-8 sm:pt-40 sm:ml-100 pt-100 
      mb-4 ml-32 block">
     <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 ">
-        <h1 className="text-4xl text-white text-center">Total teacher</h1>
+        <h1 className="text-4xl text-white text-center">teacher page</h1>
     </div>
     <div className="bg-[#59B8C3] w-60 h-40 rounded-2xl pt-10 mt-2 ">
         <h1 className="text-4xl text-white text-center">Members</h1>
